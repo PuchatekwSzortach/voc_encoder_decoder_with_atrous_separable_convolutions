1dda287f2435b6beba9cb14b9b843f1996da8505
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2020-10-08 22:42:31 +0900

    Added mlflow logging for both training and analysis
    Rough, but functional

--- net/invoke/ml.py
+++ net/invoke/ml.py
@@ -15,6 +15,7 @@ def train(_context, config_path):
         config_path (str): path to configuration file
     """
 
+    import mlflow
     import tensorflow as tf
 
     import net.data
@@ -23,77 +24,84 @@ def train(_context, config_path):
 
     config = net.utilities.read_yaml(config_path)
 
-    trainig_voc_samples_data_loader = net.data.VOCSamplesDataLoader(
-        images_directory=config["voc_data_images_directory"],
-        segmentations_directory=config["voc_data_segmentations_directory"],
-        data_set_path=config["voc_training_samples_list_path"],
-        batch_size=config["batch_size"],
-        shuffle=True
-    )
+    mlflow.set_tracking_uri(config["mlflow_tracking_uri"])
+    mlflow.set_experiment("training")
 
-    training_samples_data_loader = net.data.TrainingDataLoader(
-        samples_data_loader=trainig_voc_samples_data_loader,
-        use_training_mode=True,
-        size=config["training_image_dimension"],
-        categories=config["categories"]
-    )
+    with mlflow.start_run(run_name="simple_run"):
 
-    training_dataset = tf.data.Dataset.from_generator(
-        generator=lambda: iter(training_samples_data_loader),
-        output_types=(tf.float32, tf.float32, tf.float32),
-        output_shapes=(
-            tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"], 3]),
-            tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]),
-            tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]))
-    ).prefetch(10)
+        mlflow.tensorflow.autolog(every_n_iter=1)
 
-    validation_voc_samples_data_loader = net.data.VOCSamplesDataLoader(
-        images_directory=config["voc_data_images_directory"],
-        segmentations_directory=config["voc_data_segmentations_directory"],
-        data_set_path=config["voc_validation_samples_list_path"],
-        batch_size=config["batch_size"],
-        shuffle=False
-    )
+        trainig_voc_samples_data_loader = net.data.VOCSamplesDataLoader(
+            images_directory=config["voc_data_images_directory"],
+            segmentations_directory=config["voc_data_segmentations_directory"],
+            data_set_path=config["voc_training_samples_list_path"],
+            batch_size=config["batch_size"],
+            shuffle=True
+        )
 
-    validation_samples_data_loader = net.data.TrainingDataLoader(
-        samples_data_loader=validation_voc_samples_data_loader,
-        use_training_mode=False,
-        size=config["training_image_dimension"],
-        categories=config["categories"]
-    )
+        training_samples_data_loader = net.data.TrainingDataLoader(
+            samples_data_loader=trainig_voc_samples_data_loader,
+            use_training_mode=True,
+            size=config["training_image_dimension"],
+            categories=config["categories"]
+        )
 
-    validation_dataset = tf.data.Dataset.from_generator(
-        generator=lambda: iter(validation_samples_data_loader),
-        output_types=(tf.float32, tf.float32, tf.float32),
-        output_shapes=(
-            tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"], 3]),
-            tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]),
-            tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]))
-    ).prefetch(10)
+        training_dataset = tf.data.Dataset.from_generator(
+            generator=lambda: iter(training_samples_data_loader),
+            output_types=(tf.float32, tf.float32, tf.float32),
+            output_shapes=(
+                tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"], 3]),
+                tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]),
+                tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]))
+        ).prefetch(10)
 
-    model = net.ml.NewDeepLabBuilder().get_model(categories_count=len(config["categories"]))
+        validation_voc_samples_data_loader = net.data.VOCSamplesDataLoader(
+            images_directory=config["voc_data_images_directory"],
+            segmentations_directory=config["voc_data_segmentations_directory"],
+            data_set_path=config["voc_validation_samples_list_path"],
+            batch_size=config["batch_size"],
+            shuffle=False
+        )
 
-    model.fit(
-        x=training_dataset,
-        epochs=100,
-        steps_per_epoch=len(training_samples_data_loader),
-        validation_data=validation_dataset,
-        validation_steps=len(validation_samples_data_loader),
-        callbacks=[
-            tf.keras.callbacks.ModelCheckpoint(
-                filepath=config["current_model_directory"],
-                save_best_only=True,
-                save_weights_only=False,
-                verbose=1),
-            tf.keras.callbacks.EarlyStopping(
-                patience=4,
-                verbose=1),
-            tf.keras.callbacks.ReduceLROnPlateau(
-                factor=0.1,
-                patience=3,
-                verbose=1),
-            tf.keras.callbacks.CSVLogger(
-                filename=config["training_metrics_log_path"]
-            )
-        ]
-    )
+        validation_samples_data_loader = net.data.TrainingDataLoader(
+            samples_data_loader=validation_voc_samples_data_loader,
+            use_training_mode=False,
+            size=config["training_image_dimension"],
+            categories=config["categories"]
+        )
+
+        validation_dataset = tf.data.Dataset.from_generator(
+            generator=lambda: iter(validation_samples_data_loader),
+            output_types=(tf.float32, tf.float32, tf.float32),
+            output_shapes=(
+                tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"], 3]),
+                tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]),
+                tf.TensorShape([None, config["training_image_dimension"], config["training_image_dimension"]]))
+        ).prefetch(10)
+
+        model = net.ml.NewDeepLabBuilder().get_model(categories_count=len(config["categories"]))
+
+        model.fit(
+            x=training_dataset,
+            epochs=100,
+            steps_per_epoch=len(training_samples_data_loader),
+            validation_data=validation_dataset,
+            validation_steps=len(validation_samples_data_loader),
+            callbacks=[
+                tf.keras.callbacks.ModelCheckpoint(
+                    filepath=config["current_model_directory"],
+                    save_best_only=True,
+                    save_weights_only=False,
+                    verbose=1),
+                tf.keras.callbacks.EarlyStopping(
+                    patience=4,
+                    verbose=1),
+                tf.keras.callbacks.ReduceLROnPlateau(
+                    factor=0.1,
+                    patience=3,
+                    verbose=1),
+                tf.keras.callbacks.CSVLogger(
+                    filename=config["training_metrics_log_path"]
+                )
+            ]
+        )
